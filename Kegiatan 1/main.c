488c4aabf94c708792c14ed349741e941c896202
Muhammad Novel <92927868+muhammadnovel@users.noreply.github.com>
2021-11-28 14:54:31 +0700

    Update main.c

--- Kegiatan 1/main.c
+++ Kegiatan 1/main.c
@@ -1,75 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
 
-void printArray(int array[], int length){
+int main(void){
 
-    for (int i = 0; i < length; i++) {
-        printf("\t   %d\t\t|", array[i]);
+    int boruto[3] = {85,90,86};
+    int sarada[3] = {84,90,89};
+
+    printf("[+] Nilai Boruto : \n\n");
+
+    for(int i = 0; i < 3; i++){
+        printf("Nilai progdas ke %d : %d\n", i+1, boruto[i]);
     }
-    printf("\n ===============================================================================================\n");
-}
 
-void header(){
+    printf("\n");
 
-    printf(" /$$$$$$$                   /$$        /$$     /$$ /$$             /$$      /$$                 /$$           /$$       /$$   /$$\n");
-    printf("| $$__  $$                 | $$       | $$    |__/| $$            | $$$    /$$$                | $$          | $$      | $$  | $$\n");
-    printf("| $$  \\ $$/$$$$$$  /$$$$$$ | $$   /$$/$$$$$$   /$$| $$   /$$      | $$$$  /$$$$  /$$$$$$   /$$$$$$$ /$$   /$$| $$      | $$  | $$\n");
-    printf("| $$$$$$$/$$__  $$|____  $$| $$  /$$/_  $$_/  | $$| $$  /$$/      | $$ $$/$$ $$ /$$__  $$ /$$__  $$| $$  | $$| $$      | $$$$$$$$\n");
-    printf("| $$____/ $$  \\__/ /$$$$$$$| $$$$$$/  | $$    | $$| $$$$$$/       | $$  $$$| $$| $$  \\ $$| $$  | $$| $$  | $$| $$      |_____  $$\n");
-    printf("| $$    | $$      /$$__  $$| $$_  $$  | $$ /$$| $$| $$_  $$       | $$\\  $ | $$| $$  | $$| $$  | $$| $$  | $$| $$            | $$\n");
-    printf("| $$    | $$     |  $$$$$$$| $$ \\  $$ |  $$$$/| $$| $$ \\  $$      | $$ \\/  | $$|  $$$$$$/|  $$$$$$$|  $$$$$$/| $$            | $$\n");
-    printf("|__/    |__/      \\_______/|__/  \\__/  \\___/  |__/|__/  \\__/      |__/     |__/ \\______/  \\_______/ \\______/ |__/            |__/\n");
-    printf("======================================================================================================================================\n");
-    printf("Nama\t: Muhammad Novel\n");
-    printf("Kelas\t: Informatika G\n");
-    printf("NIM\t: 202110370311320\n");
-    printf("Aslab\t: 202110370311320\n");
-    printf("======================================================================================================================================\n");
+    printf("[+] Nilai Sarada : \n\n");
 
-}
-
-int main(void) {
-    SetConsoleTitle("Tugas Modul 4");
-    header();
-    printf("Klik enter untuk menjalankan program..."); getchar();
-    system("cls");
-
-    int boruto[] = {
-        85,
-        90,
-        86
-    };
-    int sarada[] = {
-        84,
-        90,
-        89
-    };
-
-    const int length = sizeof(boruto) / 4;
-
-    printf("\t\t\t\tTabel Nilai Ujian Pemrograman Dasar\n");
-    printf("\n ===============================================================================================\n");
-    printf("|\tNama\t\t|\tUjian 1\t\t|\tUjian 2\t\t|\tUjian 3\t\t|\n");
-    printf(" ===============================================================================================\n");
-    printf("|\tBoruto\t\t|");
-    printArray(boruto, length);
-    printf("|\tSarada\t\t|");
-    printArray(sarada, length);
-
-    printf("\n\n");
-
-    printf("\t    Tabel Perbandingan Nilai Ujian Boruto & Sarada\n");
-    printf("\n =======================================================================\n");
-    printf("|\tUjian 1\t\t|\tUjian 2\t\t|\tUjian 3\t\t|\n");
-    printf(" =======================================================================\n");
-    for (int i = 0; i < length; i++) {
-        printf("|\t   %d\t\t", (boruto[i] > sarada[i]) ? 1 : 0);
+    for(int i = 0; i < 3; i++){
+        printf("Nilai progdas ke %d : %d\n", i+1, sarada[i]);
     }
-    printf("|\n =======================================================================\n\n");
-    printf("*1 = Nilai Boruto lebih besar dari sarada\n");
-    printf("*0 = Nilai Boruto sama dengan nilai sarada atau kurang dari sarada\n\n");
 
-    printf("[!] Klik enter untuk mengakhiri program..."); getchar();
-    return 0;
+    printf("\n");
+
+    printf("[+] Perbandingan Nilai Boruto & Sarada :\n\n");
+
+     for(int i = 0; i < 3; i++){
+       if(boruto[i] > sarada[i]){
+            printf("Perbandingan nilai ke %d : 1\n", i+1);
+       }else {
+            printf("Perbandingan nilai ke %d : 0\n", i+1);
+       }
+    }
 }
